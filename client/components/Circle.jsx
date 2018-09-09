383469f7dc3d4082df81b3ec10abfc029e9ed27f
Cate Palmer <catherinepalmer@gmail.com>
2018-09-09 12:36:48 +1200

    Tried to add rough version of child circle elements to handleMouseOver function

--- client/components/Circle.jsx
+++ client/components/Circle.jsx
@@ -10,15 +10,65 @@ class Circle extends React.Component {
         cy: props.height / 2,
         level: 0,
         r: 256,
-        hasChildren: false
+        hasChildren: false,
+        children: []
       }
     }
+    this.handleMouseOver = this.handleMouseOver.bind(this)
+  }
+
+  handleMouseOver () {
+      this.setState({
+          hasChildren: true,
+          children: [
+              {
+                circle: {
+                    cx: this.state.cx,
+                    cy: this.state.cy - this.state.r,
+                    level: this.state.level + 1,
+                    r: this.state.r / 2,
+                    hasChildren: false,
+                    children: []
+                  }
+              },
+              {
+                circle: {
+                    cx: this.state.cx + this.state.r,
+                    cy: this.state.cy,
+                    level: this.state.level + 1,
+                    r: this.state.r / 2,
+                    hasChildren: false,
+                    children: []
+                  }
+              },
+              {
+                circle: {
+                    cx: this.state.cx,
+                    cy: this.state.cy + this.state.r,
+                    level: this.state.level + 1,
+                    r: this.state.r / 2,
+                    hasChildren: false,
+                    children: []
+                  }
+              },
+              {
+                circle: {
+                    cx: this.state.cx - this.state.r,
+                    cy: this.state.cy,
+                    level: this.state.level + 1,
+                    r: this.state.r / 2,
+                    hasChildren: false,
+                    children: []
+                  }
+              }
+          ]
+      })
   }
 
   render() {
     return (
       <svg width={this.props.width} height={this.props.height} >
-        <circle cx={this.state.circle.cx} cy={this.state.circle.cy} r={this.state.circle.r} />
+        <circle cx={this.state.circle.cx} cy={this.state.circle.cy} r={this.state.circle.r} mouseover={this.handleMouseOver} />
       </svg>
     )
   }
